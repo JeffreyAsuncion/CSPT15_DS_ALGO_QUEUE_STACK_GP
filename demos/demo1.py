23831ad33b2938df7911f7b8c467bacf0adbf3de
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-12-30 10:43:03 -0500

    demo 01 - max stack

--- demos/demo1.py
+++ demos/demo1.py
@@ -1,13 +1,22 @@
 """
-You've encountered a situation where you want to easily be able to pull the
+You've encountered a situation 
+where you want to easily be able to pull the
 largest integer from a stack.
-You already have a `Stack` class that you've implemented using a dynamic array.
-Use this `Stack` class to implement a new class `MaxStack` with a method
-`get_max()` that returns the largest element in the stack. `get_max()` should
-not remove the item.
-*Note: Your stacks will contain only integers. You should be able to get a
+
+You already have a `Stack` class 
+that you've implemented using a dynamic array.
+Use this `Stack` class to implement a new class 
+`MaxStack` with a method
+`get_max()` that 
+returns the largest element in the stack. 
+
+`get_max()` should not remove the item.
+
+*Note: Your stacks will contain only integers. 
+You should be able to get a
 runtime of O(1) for push(), pop(), and get_max().*
 """
+
 class Stack:
     def __init__(self):
         """Initialize an empty stack"""
@@ -32,10 +41,15 @@ class Stack:
             return None
         return self.items[-1]
 
+
 class MaxStack:
     def __init__(self):
         # Your code here
-        pass
+        # keep track of a normal stack to the standard operations
+        self.stack = Stack()
+        # keep track of a max stack (which is just another normal stack) 
+        # that we put the biggest number at the top every push
+        self.max_stack = Stack() 
 
 
     def push(self, item):
@@ -47,14 +61,25 @@ class MaxStack:
         # check if our max stack is empty?
         # or if our item is larger thant the top of our max stack
         if self.max_stack.peek() is None or item >= self.max_stack.peek():
-            max_stage.push(item)
+            self.max_stack.push(item)
 
 
     def pop(self):
         """Remove and return the top item from our stack."""
         # Your code here
+        # do the normal pop operation storing the value that was popped off
+        item = self.stack.pop()
+
+        # or if our item is larger thant the top of our max stack
+        if item == self.max_stack.peek():
+            self.max_stack.pop()
+
+        # return the item to the caller
+        return item
 
 
     def get_max(self):
-        """The last item in maxes_stack is the max item in our stack."""
+        """The last item in max_stack is the max item in our stack."""
         # Your code here
+        # peek the top of the max stack
+        return self.max_stack.peek()
